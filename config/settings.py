ff272f55f8a7dd0e18830dc2f010a251ddf348e2
Parsa Rasoli <rasooliparsa209@gmail.com>
2026-07-26 23:46:10 +0330

    add login and signup

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 
 # accounts config 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- pages/tests.py
+++ pages/tests.py
@@ -1,2 +1,46 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+
+User = get_user_model()
+
+
+class PageTests(TestCase):
+
+    # =========================
+    # Status Code Tests
+    # =========================
+
+    def test_home_page_status_code(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_signup_page_status_code(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_login_page_status_code(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+    # =========================
+    # Template Tests
+    # =========================
+
+    def test_home_page_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
+
+
+    def test_signup_page_template(self):
+        response = self.client.get(reverse('signup'))
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+
+    def test_login_page_template(self):
+        response = self.client.get(reverse('login'))
+        self.assertTemplateUsed(response, 'registration/login.html')
+
 
